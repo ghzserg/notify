c7c0fe7a9ab7e5732400b2a7480aeb2839f210be
Sergei Rozhkov <fgup@zserg.ru>
2025-12-17 09:27:30 +0500

    Update

--- update.sh
+++ update.sh
@@ -6,7 +6,3 @@ INC="[include plugins/notify/${ZLANG}/notify.moonraker.cfg]"
 FILE="/opt/config/mod_data/plugins.moonraker.conf"
 
 grep -qF "${INC}" "${FILE}" || echo "${INC}" >> "${FILE}"
-
-[ -f /root/printer_data/moonraker.secrets ] || echo "[notify]
-url: tgram://{bottoken}/{ChatID}
-" >/root/printer_data/moonraker.secrets
